574eb124fd8672919dd7cde28e6a9cb00c409f69
Daniel Wilson-Thomas <daniel@mojotech.com>
2019-01-13 13:44:33 -0500

    Fix typo

--- src/cheat-sheet/conflicts.tsx
+++ src/cheat-sheet/conflicts.tsx
@@ -5,7 +5,7 @@ export default () => (
     <h2>Conflicts</h2>
     <ul>
       <li>
-        Set the scene, describing the environ- ment the conflict takes place in,
+        Set the scene, describing the environment the conflict takes place in,
         cre- ating situation aspects and zones, and establishing who’s
         participating and what side they’re on.
       </li>
