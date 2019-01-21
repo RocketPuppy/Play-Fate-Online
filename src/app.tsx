50480d5a3393759125de0975e438fc9611dd380a
Daniel Wilson-Thomas <daniel@mojotech.com>
2019-01-20 19:12:21 -0500

    Wire up some basic navigation

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link, Route } from "react-router-dom";
 
 import CharacterCreation from "./character-creation";
 import CharacterSheet from "./character-sheet";
@@ -6,8 +7,11 @@ import CheatSheet from "./cheat-sheet";
 
 export default () => (
   <div>
-    <CharacterCreation />
-    <CheatSheet />
-    <CharacterSheet />
+    <Link to="/create">Create your character</Link>
+    <Link to="/cheat-sheet">Cheat sheet</Link>
+    <Link to="/character">Your character</Link>
+    <Route path="/create" component={CharacterCreation} />
+    <Route path="/cheat-sheet" component={CheatSheet} />
+    <Route path="/character" component={CharacterSheet} />
   </div>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById("inject-here"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("inject-here")
+);
